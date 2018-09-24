d45f26cd986f104c270d3558f538c6bccc8f6f5b
Han Zheng <hzheng12@stevens.edu>
2018-09-24 15:30:10 -0400

    Nothing

--- QtClient/QtClient/src/CandlestickChart/candledataset.cpp
+++ QtClient/QtClient/src/CandlestickChart/candledataset.cpp
@@ -10,18 +10,11 @@ CandleDataSet::CandleDataSet(QString symbol)
 {
 }
 
-/**
- * @brief Method to return a QwtOHLCSample(candledatasample) by looking for the
- *        target timestamp. (Only used when show tracker text)
- * @param timestamp The timestamp of the target sample in long long type.
- * @param sample The address/reference of the returned sample.
- * @return bool If the sample is found, return true, otherwise return false.
- */
 bool CandleDataSet::getSampleByTimestamp(long long timestamp, QwtOHLCSample& sample)
 {
     if (d_samples.size() > 1) {
         long long firstTime = d_samples[0].time;
-        long long timeInterval = d_samples[1].time - d_samples[0].time;
+        long long timeInterval = d_samples[1].time - firstTime;
         int index = (timestamp - firstTime + timeInterval / 2) / timeInterval;
         if (index >= 0 && index < d_samples.size()) {
             sample = d_samples[index];
