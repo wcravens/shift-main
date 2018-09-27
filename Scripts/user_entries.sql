0d3ebbf9fe8f02b55ea27415d1d96177fd704345
weipu <wxu16@stevens.edu>
2018-09-27 16:39:32 -0400

    remove commented code in u_e.sql

--- Scripts/user_entries.sql
+++ Scripts/user_entries.sql
@@ -49,38 +49,5 @@ BEGIN
 
 END $$;
 
----------------------------------------------------------------
-
--- Table: public.trade_record
-
--- DROP TABLE IF EXISTS public.trade_record;
-
--- CREATE TABLE public.trade_record
--- (
---   real_time timestamp without time zone,
---   execute_time timestamp without time zone,
---   symbol character varying(15),
---   price real,
---   size integer,
---   trader_id_1 bigint NOT NULL,
---   trader_id_2 bigint NOT NULL,
---   order_id_1 character varying(20) NOT NULL,
---   order_id_2 character varying(20),
---   order_type_1 character varying(2),
---   order_type_2 character varying(2),
---   time1 time without time zone,
---   time2 time without time zone,
---   decision character varying(10),
---   destination character varying(10),
---   CONSTRAINT trade_record_pkey PRIMARY KEY (order_id_1),
---   CONSTRAINT trade_record_fkey FOREIGN KEY (trader_id_1)
---       REFERENCES public.traders (id) MATCH SIMPLE
---       ON UPDATE NO ACTION ON DELETE NO ACTION
--- )
--- WITH (
---   OIDS=FALSE
--- );
--- ALTER TABLE public.trade_record
---   OWNER TO hanlonpgsql4;
 
 ---------------------------------------------------------------
