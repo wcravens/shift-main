6b2f6ad2c0e418a175f29b189fc76a5209e79b41
weipu <wxu16@stevens.edu>
2018-11-29 14:17:03 -0500

    change back to uppercase keywords

--- Scripts/bc_instances.sql
+++ Scripts/bc_instances.sql
@@ -12,61 +12,66 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 ---------------------------------------------------------------
 
--- Table: public.buying_power
+-- Table: PUBLIC.buying_power
 
--- DROP TABLE public.buying_power;
+-- DROP TABLE PUBLIC.buying_power;
 
-CREATE TABLE public.buying_power
+CREATE TABLE PUBLIC.buying_power
 (
-  tb_clientid character varying NOT NULL,
-  tb_bp double precision,
+  tb_clientid VARCHAR NOT NULL,
+  tb_bp DOUBLE PRECISION,
   CONSTRAINT buying_power_pkey PRIMARY KEY (tb_clientid)
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE public.buying_power
+ALTER TABLE PUBLIC.buying_power
   OWNER TO hanlonpgsql4;
 
 ---------------------------------------------------------------
 
--- Table: public.holdings_xyz
+-- Table: PUBLIC.holdings_xyz
 
--- DROP TABLE public.holdings_xyz;
+-- DROP TABLE PUBLIC.holdings_xyz;
 
-CREATE TABLE public.holdings_xyz
+CREATE TABLE PUBLIC.holdings_xyz
 (
-  ht_clientid character varying NOT NULL,
-  ht_shares double precision,
-  ht_avgprice double precision,
+  ht_clientid VARCHAR NOT NULL,
+  ht_shares DOUBLE PRECISION,
+  ht_avgprice DOUBLE PRECISION,
   CONSTRAINT holdings_xyz_pkey PRIMARY KEY (ht_clientid)
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE public.holdings_xyz
+ALTER TABLE PUBLIC.holdings_xyz
   OWNER TO hanlonpgsql4;
 
 ---------------------------------------------------------------
 
--- Table: public.web_traders
+-- Table: PUBLIC.web_traders
 
--- DROP TABLE public.web_traders;
+-- DROP TABLE PUBLIC.web_traders;
 
-CREATE TABLE public.web_traders
+CREATE TABLE PUBLIC.web_traders
 (
   id SERIAL,
-  username character varying(40) NOT NULL,
-  password character varying(40) NOT NULL,
-  role character varying(20) NOT NULL DEFAULT 'student'::character varying,
-  email character varying(40) NOT NULL,
-  sessionid character varying(40),
+  portfolio_id INTEGER DEFAULT -1,
+  username VARCHAR(40) NOT NULL UNIQUE,
+  password VARCHAR(40) NOT NULL,
+  role VARCHAR(20) NOT NULL DEFAULT 'student'::VARCHAR,
+  email VARCHAR(40) NOT NULL,
+  sessionid VARCHAR(40),
+
   CONSTRAINT web_traders_pkey PRIMARY KEY (id)
+  -- CONSTRAINT web_traders_fkey FOREIGN KEY (portfolio_id)
+  --   REFERENCES PUBLIC.portfolio_summary (portfolio_id) MATCH SIMPLE
+  --   ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE public.web_traders
+ALTER TABLE PUBLIC.web_traders
   OWNER TO hanlonpgsql4;
 
 ---------------------------------------------------------------
@@ -77,14 +82,15 @@ ALTER TABLE public.web_traders
 
 CREATE TABLE public.new_traders
 (
-  id uuid DEFAULT uuid_generate_v4(),
-  username character varying(40) NOT NULL,
-  password character varying(40) NOT NULL,
-  firstname character varying(40) NOT NULL,
-  lastname character varying(40) NOT NULL,
-  email character varying(40) NOT NULL,
-  role character varying(20) NOT NULL DEFAULT 'student'::character varying,
-  sessionid character varying(40),
+  id UUID DEFAULT uuid_generate_v4(),
+  username VARCHAR(40) NOT NULL,
+  password VARCHAR(40) NOT NULL,
+  firstname VARCHAR(40) NOT NULL,
+  lastname VARCHAR(40) NOT NULL,
+  email VARCHAR(40) NOT NULL,
+  role VARCHAR(20) NOT NULL DEFAULT 'student'::VARCHAR,
+  sessionid VARCHAR(40),
+
   CONSTRAINT new_traders_pkey PRIMARY KEY (id)
 )
 WITH (
