6a8fa822910b1482eb34abfe3fead98b948d8403
weipu <wxu16@stevens.edu>
2019-03-27 12:58:04 -0400

    better performance of synchPrint() when no -v

--- LibMiscUtils/src/terminal/Common.cpp
+++ LibMiscUtils/src/terminal/Common.cpp
@@ -6,8 +6,10 @@ static std::mutex s_shiftTerminalMutex;
 
 void synchPrint(const char* msg)
 {
-    std::lock_guard<std::mutex> guard(s_shiftTerminalMutex);
-    cout << msg << flush;
+    if (cout) { // == true iff. the status flags of cout is cleared; the mutex locking incurs overhead but we don't need such locking anyway if no --verbose option set; see VerboseOptHelper's impl.
+        std::lock_guard<std::mutex> guard(s_shiftTerminalMutex);
+        cout << msg << flush;
+    }
 }
 
 void synchPrint(const std::string& msg)
