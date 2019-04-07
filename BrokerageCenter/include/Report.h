41c9625106087a50ed139789338f912492ed3cfc
Thiago W. Alves <twinkle1@stevens.edu>
2019-04-06 21:10:22 -0400

    Standardizing constructors in BC

--- BrokerageCenter/include/Report.h
+++ BrokerageCenter/include/Report.h
@@ -16,4 +16,52 @@ struct Report {
     std::string destination;
     FIX::UtcTimeStamp execTime;
     FIX::UtcTimeStamp serverTime;
+
+    Report(const std::string& username,
+        const std::string& orderID,
+        Order::Type orderType,
+        const std::string& orderSymbol,
+        int currentSize,
+        int executedSize,
+        double orderPrice,
+        Order::Status orderStatus,
+        const std::string& destination,
+        const FIX::UtcTimeStamp& execTime,
+        const FIX::UtcTimeStamp& serverTime)
+        : username(username)
+        , orderID(orderID)
+        , orderType(orderType)
+        , orderSymbol(orderSymbol)
+        , currentSize(currentSize)
+        , executedSize(executedSize)
+        , orderPrice(orderPrice)
+        , orderStatus(orderStatus)
+        , destination(destination)
+        , execTime(execTime)
+        , serverTime(serverTime)
+    {
+    }
+
+    Report(const std::string& username,
+        const std::string& orderID,
+        Order::Type orderType,
+        const std::string& orderSymbol,
+        int currentSize,
+        int executedSize,
+        double orderPrice,
+        Order::Status orderStatus,
+        const std::string& destination)
+        : username(username)
+        , orderID(orderID)
+        , orderType(orderType)
+        , orderSymbol(orderSymbol)
+        , currentSize(currentSize)
+        , executedSize(executedSize)
+        , orderPrice(orderPrice)
+        , orderStatus(orderStatus)
+        , destination(destination)
+        , execTime()
+        , serverTime()
+    {
+    }
 };
--- BrokerageCenter/src/OrderBookEntry.cpp
+++ BrokerageCenter/src/OrderBookEntry.cpp
@@ -1,13 +1,13 @@
 #include "OrderBookEntry.h"
 
 OrderBookEntry::OrderBookEntry(OrderBookEntry::Type type, const std::string& symbol, double price, int size, const std::string& destination, const FIX::UtcDateOnly& date, const FIX::UtcTimeOnly& time)
-    : m_type{ type }
-    , m_symbol{ symbol }
-    , m_price{ price }
-    , m_size{ size }
-    , m_destination{ destination }
-    , m_date{ date }
-    , m_time{ time }
+    : m_type(type)
+    , m_symbol(symbol)
+    , m_price(price)
+    , m_size(size)
+    , m_destination(destination)
+    , m_date(date)
+    , m_time(time)
 {
 }
 
