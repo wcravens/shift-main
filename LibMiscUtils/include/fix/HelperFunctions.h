7a44800d441959beacd0facee2c447c9fe43e432
jduan <jduan8@stevens.edu>
2019-05-16 11:52:05 -0400

    Use rvalue reference in FIX HelperFunctions

--- LibMiscUtils/include/fix/HelperFunctions.h
+++ LibMiscUtils/include/fix/HelperFunctions.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "../MiscUtils_EXPORTS.h"
 
 namespace shift {
@@ -12,24 +14,24 @@ namespace fix {
         }
 
         template <typename GroupType, typename FieldType, typename... Args>
-        GroupType createFIXGroupImpl(GroupType& g, FieldType f, Args... args)
+        GroupType createFIXGroupImpl(GroupType& g, const FieldType& f, Args&&... args)
         {
             g.setField(f);
-            return createFIXGroupImpl(g, args...);
+            return createFIXGroupImpl(g, std::forward<Args>(args)...);
         }
     } // namespace details
 
     template <typename GroupType, typename... Args>
-    GroupType createFIXGroup(Args... args)
+    GroupType createFIXGroup(Args&&... args)
     {
         GroupType g;
-        return details::createFIXGroupImpl(g, args...);
+        return details::createFIXGroupImpl(g, std::forward<Args>(args)...);
     }
 
     template <typename GroupType, typename MsgType, typename... Args>
-    void addFIXGroup(MsgType& msg, Args... args)
+    void addFIXGroup(MsgType& msg, Args&&... args)
     {
-        msg.addGroup(createFIXGroup<GroupType>(args...));
+        msg.addGroup(createFIXGroup<GroupType>(std::forward<Args>(args)...));
     }
 
 } // fix
