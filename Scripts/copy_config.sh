7695207223bb1f65337b59029313032f7c883f3f
Thiago Alves <twinkle1@stevens.edu>
2019-10-13 10:04:27 -0400

    Fixing copy_config.sh

--- Scripts/copy_config.sh
+++ Scripts/copy_config.sh
@@ -3,25 +3,17 @@
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Brokerage Center
-if [ ! -d ${SCRIPTS_DIR}/../BrokerageCenter/config ]
-then
-    cp -r ${SCRIPTS_DIR}/../BrokerageCenter/config.default ${SCRIPTS_DIR}/../BrokerageCenter/config
-fi
+[ -d ${SCRIPTS_DIR}/../BrokerageCenter/config ] && rm -r ${SCRIPTS_DIR}/../BrokerageCenter/config
+cp -r ${SCRIPTS_DIR}/../BrokerageCenter/config.default ${SCRIPTS_DIR}/../BrokerageCenter/config
 
 # Datafeed Engine
-if [ ! -d ${SCRIPTS_DIR}/../DatafeedEngine/config ]
-then
-    cp -r ${SCRIPTS_DIR}/../DatafeedEngine/config.default ${SCRIPTS_DIR}/../DatafeedEngine/config
-fi
+[ -d ${SCRIPTS_DIR}/../DatafeedEngine/config ] && rm -r ${SCRIPTS_DIR}/../DatafeedEngine/config
+cp -r ${SCRIPTS_DIR}/../DatafeedEngine/config.default ${SCRIPTS_DIR}/../DatafeedEngine/config
 
 # Matching Engine
-if [ ! -d ${SCRIPTS_DIR}/../MatchingEngine/config ]
-then
-    cp -r ${SCRIPTS_DIR}/../MatchingEngine/config.default ${SCRIPTS_DIR}/../MatchingEngine/config
-fi
+[ -d ${SCRIPTS_DIR}/../MatchingEngine/config ] && rm -r ${SCRIPTS_DIR}/../MatchingEngine/config
+cp -r ${SCRIPTS_DIR}/../MatchingEngine/config.default ${SCRIPTS_DIR}/../MatchingEngine/config
 
 # Web Client
-if [ ! -d ${SCRIPTS_DIR}/../WebClient/config ]
-then
-    cp -r ${SCRIPTS_DIR}/../WebClient/config.default ${SCRIPTS_DIR}/../WebClient/config
-fi
+[ -d ${SCRIPTS_DIR}/../WebClient/config ] && rm -r ${SCRIPTS_DIR}/../WebClient/config
+cp -r ${SCRIPTS_DIR}/../WebClient/config.default ${SCRIPTS_DIR}/../WebClient/config
