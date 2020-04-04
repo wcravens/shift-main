94850c3e277429a9e9b9a28d3f7ca6e772c7b7e8
Jose Uribe <jseuribe@hotmail.com>
2020-04-04 02:05:05 +0000

    added tests

---
+++ WebClient/backend/service/thrift/tests/testChangepw.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append("shiftpy")
+
+from shift_service import SHIFTService
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+
+trans = TSocket.TSocket('localhost', 9090)
+trans = TTransport.TBufferedTransport(trans)
+
+proto = TBinaryProtocol.TBinaryProtocol(trans)
+client = SHIFTService.Client(proto)
+
+trans.open()
+
+msg = None
+
+#msg = client.is_login("e84cbe1b-300e-4b3e-8174-b7125297fa45")
+msg = client.change_password('password134','password134','testX')
+
+print(msg)
