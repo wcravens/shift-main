3aa504425c2be8944e36880ea15b9be578a88170
Thiago Alves <twinkle1@stevens.edu>
2020-04-26 17:34:03 -0400

    Removing unnecessary namespace use

--- MatchingEngine/src/configFunctions.cpp
+++ MatchingEngine/src/configFunctions.cpp
@@ -5,8 +5,6 @@
 
 #include <shift/miscutils/terminal/Common.h>
 
-using namespace std::chrono_literals;
-
 bool fileConfigMode(std::string filePath, std::string& date, std::string& startTime, std::string& endTime, int& experimentSpeed, std::vector<std::string>& symbols)
 {
     std::string line = "";
